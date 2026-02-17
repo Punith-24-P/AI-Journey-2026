ba1adbcb70c7e5cae34a1aeac6570985165dd0f5
Punith K K <punith14bms@gmail.com>
2026-02-18 01:25:36 +0530

    Added Python folder to public repo

---
+++ 01_python/day1.py
@@ -0,0 +1,29 @@
+print("Day 1 - Restarting AI Journey")
+
+name = input("Enter your name:")
+age = int(input("Enter your age"))
+
+print("Hello",name)
+print("Next year you will be",age + 1)
+
+
+
+
+num = int(input("Enter a number:"))
+
+if num% 2 == 0:
+    print("Even")
+else:
+    print("odd")
+    
+    
+    
+num = int(input("Enter number to square"))
+print("square is:",num * num)
+    
+    
+    
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+print("Sum is:",a + b)
