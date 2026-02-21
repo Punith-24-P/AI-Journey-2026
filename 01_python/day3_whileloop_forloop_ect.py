623cba9d504e5e7ff174c23c5d20aadde17cfe3c
Punith K K <punith14bms@gmail.com>
2026-02-21 23:53:57 +0530

    Day 3 of whileloop and forloop

---
+++ 01_python/day3_whileloop_forloop_ect.py
@@ -0,0 +1,75 @@
+for num in range(1, 11):
+    print(num)
+    
+    
+for t in range(2, 12):
+    print(t)
+    
+for i in range(1, 11, 2):
+    print(i)
+    
+    
+for i in range(5):
+    print("inside the loop")
+    
+    print("out side the for loop")
+    
+    
+for i in range(5):
+    print("inside the loop")
+    
+print("out side the for loop")
+
+
+for i in range(5):
+    print("inside the loop")
+    
+    print("out side the for loop")
+    
+name = "punith"
+age = 21
+print(name,age)
+
+name = "punith"
+age = 21
+print(f"my name is {name} and i am {age} years old")
+
+for i in range(1, 6):
+    print(f"Number is {i}")
+    
+    
+
+
+for i in range(1, 5):
+    print(f"loop running{i}")
+    
+print(f"loop finished {i}")
+
+i = 1
+while i <= 20:
+    print(i)
+    
+    i += 1
+    
+i = 2
+while i <= 50:
+    print(i)
+    
+    i += 2
+    
+    
+n = int(input("enter the number:"))
+
+total = 0
+for i in range(1,n + 1):
+    total += i
+    
+print("sum is:",total)    
+
+
+
+
+i = 1
+while i <= 10:
+    print(f"7 x {i} = {7*i}")
+    i += 1
