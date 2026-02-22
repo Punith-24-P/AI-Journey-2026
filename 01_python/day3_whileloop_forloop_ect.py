3d91986b9ec5220494297f815fc873259e127355
Punith K K <punith14bms@gmail.com>
2026-02-22 22:54:40 +0530

    Day 4 of python whileloop+loop controle(break,continue)

--- 01_python/day3_whileloop_forloop_ect.py
+++ 01_python/day3_whileloop_forloop_ect.py
@@ -73,3 +73,4 @@ i = 1
 while i <= 10:
     print(f"7 x {i} = {7*i}")
     i += 1
+    
---
+++ 01_python/day4_whileloop+loop_controle(break,continue).py
@@ -0,0 +1,22 @@
+i = 1
+while i <= 20:
+    print(i)
+    
+    i += 1
+    
+    
+i = 2
+while i <= 50:
+    print(i)
+    
+    i += 2
+    
+    
+    
+i = 1
+while True:
+    print(i)
+    
+if i == 10:
+    break
+i += 1
