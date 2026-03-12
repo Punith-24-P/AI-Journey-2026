b4414c9bfded29041092b5ca512bb2b377064406
Punith K K <punith14bms@gmail.com>
2026-03-12 23:28:49 +0530

    Day3 python practice

---
+++ day3_variables.py
@@ -0,0 +1,42 @@
+name = "punith"
+print(name)
+
+name = "punith"
+age = 21
+goal = "AI Engineer"
+print(name)
+print(age)
+print(goal)
+
+name = "punith"#text
+age = 20 #number
+height = 5.8#decimal
+
+name = input("enter your name:")
+print("hello",name)
+
+a = 10
+b = 5
+
+result = a+b
+print(result)
+
+name = "punith"
+goal = "AI engineer"
+print("my name is",name)
+print("my goal is",goal)
+
+name = "punith"
+goal = "Trader"
+country = "india"
+print("name:",name)
+print("goal:",goal)
+print("country:",country)
+name = "Punith"
+age = 20
+goal = "AI Engineer"
+
+print("My name is", name)
+print("I am", age, "years old")
+print("My goal is to become an", goal)
+print("GitHub contribution test")
